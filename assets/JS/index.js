143a3bc74362b85049623de097afb1ac5533d9e9
artdup <77047929+artdup@users.noreply.github.com>
2021-03-10 16:17:16 +0100

    Updating sign in, index and player webpages, adding logo for testing

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -10,10 +10,10 @@ function submit() {
     return url;
 }
 
-function youtube_parser(url){
+function youtube_parser(url) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = url.match(regExp);
-    return (match&&match[7].length==11)? match[7] : false;
+    return (match && match[7].length == 11) ? match[7] : false;
 }
 
 button.addEventListener('click', () => {
